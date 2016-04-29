a42fa9d63be7fe89586da4120d450e98191980ad
Anthony <tonypr100@gmail.com>
2016-04-28 22:44:08 -0400

    Updates to the ocr cleaner

--- piccal/app.py
+++ piccal/app.py
@@ -1,15 +1,25 @@
 import os
 from flask import Flask, request, redirect, url_for
 from werkzeug import secure_filename
+import cv2
+from PIL import Image
+from StringIO import StringIO
+import numpy as np
+import ocr
 
 UPLOAD_FOLDER = '/tmp/'
-ALLOWED_EXTENSIONS = set(['bmp'])
+ALLOWED_EXTENSIONS = set(['bmp', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def convert_to_cv(image_data):
+    pil_image = Image.open(StringIO(image_data));
+    cv2_image = np.array(pil_image)
+    return cv2_image
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -29,8 +39,12 @@ def uploads():
     file = request.files['file']
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return redirect(url_for('index'))
+        cv2_image = convert_to_cv(file.read())
+        extracted_texts, bboxes = ocr.extract_image_text(cv2_image)
+        print(len(extracted_texts))
+        print(extracted_texts)
+        return str(extracted_texts)
+    return "Error"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
